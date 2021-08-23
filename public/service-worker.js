9e37b7a814a75cfead4cf858f463f60df8753ce8
rangamboa <rangamboa@gmail.com>
2021-08-22 22:08:01 -0700

    editing service worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -2,7 +2,9 @@ const CACHE_NAME = "static-cache-v2";
 const DATA_CACHE_NAME = "data-cache-v1";
 const FILES_TO_CACHE = [
   "/",
+  "/db.js",
   "/index.html",
+  "/index.js",
   "/manifest.webmanifest",
   "/styles.css"
 ];
